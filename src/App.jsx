28f659d9a4a436d866ff241cfdb6ec6cc62be21c
Varun Chimurkar <varunchimurkar0908@gmail.com>
2025-11-13 13:53:04 +0530

    Set up routing for user list, details, creation, and edit pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+import UserList from '../Components/UserList'
+import UserForm from '../Components/UserForm'
+import UserDetails from '../Components/UserDetails'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app'>
+      <nav></nav>
+      <main className='container'>
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/Users/new" element={<UserForm mode="create" />} />
+          <Route path="/Users/:id" element={<UserDetails />} />
+          <Route path="/Users/:id/edit" element={<UserList mode="edit" />} />
+       </Routes>
+
+      </main>
+    </div>
   )
+
+
 }
 
 export default App
